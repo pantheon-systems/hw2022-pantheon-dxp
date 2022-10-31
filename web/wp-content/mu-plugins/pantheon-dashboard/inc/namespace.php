b584d972feef0f46eb9800a22f4273fc5fa1e076
Chris Reynolds <chris@jazzsequence.com>
2022-10-31 14:08:41 -0600

    add the custom admin logo

--- web/wp-content/mu-plugins/pantheon-dashboard/inc/namespace.php
+++ web/wp-content/mu-plugins/pantheon-dashboard/inc/namespace.php
@@ -13,6 +13,7 @@ const COLOR_YELLOW = '#ffdc28';
 
 function bootstrap() {
 	add_action( 'admin_init', __NAMESPACE__ . '\\add_color_scheme', 999 );
+	add_action( 'admin_bar_menu', __NAMESPACE__ . '\\admin_bar_menu' );
 }
 
 function add_color_scheme() {
@@ -33,3 +34,27 @@ function add_color_scheme() {
 		]
 	);
 }
+
+/**
+ * Add new admin menu bar logo.
+ */
+function admin_bar_menu( WP_Admin_Bar $wp_admin_bar ) {
+	$logo_url = get_logo_url();
+	$alt_text = __( 'Pantheon Admin' );
+	// Add the new logo item.
+	$wp_admin_bar->add_menu(
+		[
+			'id' => 'pantheon',
+			'title' => "<span class=\"pantheon-logo-wrapper\"><img src=\"$logo_url\" alt=\"$alt_text\" height=\"32\" /></span><span class=\"screen-reader-text\">Pantheon</span>",
+			'href' => admin_url(),
+		]
+	);
+}
+
+/**
+ * Get the logo url.
+ */
+function get_logo_url() : string {
+	return sprintf( '%s/assets/%s', untrailingslashit( plugin_dir_url( dirname( __FILE__ ) ) ), 'pantheon-logo-white.svg' );
+}
+
